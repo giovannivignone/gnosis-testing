801f005bd3ce370f8b66609cd39cc9ff606ae0fb
Giovanni Vignone <giovanni.vignone@duke.edu>
2022-08-03 11:40:25 -0700

    working in sign typedata

--- src/App.js
+++ src/App.js
@@ -4,9 +4,25 @@ import EthersAdapter from '@gnosis.pm/safe-ethers-lib';
 import { ethers } from 'ethers'
 import { useState } from 'react';
 
+export const EIP712_SAFE_TX_TYPE = {
+  // "SafeTx(address to,uint256 value,bytes data,uint8 operation,uint256 safeTxGas,uint256 baseGas,uint256 gasPrice,address gasToken,address refundReceiver,uint256 nonce)"
+  SafeTx: [
+    { type: 'address', name: 'to' },
+    { type: 'uint256', name: 'value' },
+    { type: 'bytes', name: 'data' },
+    { type: 'uint8', name: 'operation' },
+    { type: 'uint256', name: 'safeTxGas' },
+    { type: 'uint256', name: 'baseGas' },
+    { type: 'uint256', name: 'gasPrice' },
+    { type: 'address', name: 'gasToken' },
+    { type: 'address', name: 'refundReceiver' },
+    { type: 'uint256', name: 'nonce' },
+  ],
+};
+
 function App() {
   const [buttonText, setButtonText] = useState("Connect")
-  const [provider, setProvider] = useState(null)
+  const [provider, setProvider] = useState<null | ethers.providers.Web3Provider>(null)
 
   const buttonFunction = async () => {
     if (window.web3) {
@@ -41,9 +57,18 @@ function App() {
         data: "0x"
       }
     ])
-    console.log(transaction)
+    // Above creates a batched transaction to the multiSend smart contract built by Gnosis
+
     const transactionHash = await safe.getTransactionHash(transaction)
-    
+    signer._signTypedData({
+      version: '1',
+      chainId: 4,
+      verifyingContract: '0x90fB167d002A08115638B58D2b6b50d40ee5a1d1',
+
+      },
+      EIP712_SAFE_TX_TYPE,
+      transaction
+      )
   }
   return (
     <div className="App">
